21645a8d2ea31df995dd2fc478c57edce9917615
MNesic <42974255+m2nesic@users.noreply.github.com>
2021-08-10 13:10:10 -0400

    Delete game directory

--- game/review/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-@admin.register(models.Review)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('title', 'game', 'id', 'status', 'slug', 'author', 'gold', 'silver')
-    prepopulated_fields = {'slug' : ('title',), }
--- game/review_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ReviewApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'review_api'
