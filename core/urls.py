038ef7cae3b0b75a534616459aaa148079ac62c7
MNesic <42974255+m2nesic@users.noreply.github.com>
2021-08-10 13:10:36 -0400

    Add files via upload

---
+++ core/urls.py
@@ -0,0 +1,24 @@
+
+from django import conf
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.conf import settings
+from django.conf.urls.static import static
+
+# urls used 
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('admin/', admin.site.urls),
+    path('api/', include('review_api.urls', namespace='review_api')), 
+    path('api/user/', include('users.urls', namespace='users')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('review.urls', namespace='review')),
+    
+]
+#urls used to individually view images
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
